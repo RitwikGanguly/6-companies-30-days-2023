34c795a3c76b6916ea739be653790f579b4446cb
RitwikGanguly <gangulyritwik2003@gmail.com>
2023-01-04 14:18:56 +0530

    one file is added

---
+++ Microsoft Questions/largest_divisable_subset.py
@@ -0,0 +1,18 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+
+        if len(nums) == 0:
+            return
+
+        elif len(nums) == 1:
+            return nums
+
+        nums.sort()
+        dp = [set() for _ in nums]
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 or nums[j] % nums[i] == 0:
+                    if 1 + len(dp[j]) > len(dp[i]):
+                        dp[i] = dp[j] | {nums[i]}
+            dp[i] |= {nums[i]}
+        return max(dp, key=len)
